f89b5dce05579c3d9c63856aa102db26d1bdc039
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-09 15:00:06 -0300

    t

--- api/src/server.js
+++ api/src/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(routes)
 
 app.use((error, req, res, next) => {
-  res.status(erro.status || 500)
+  res.status(error.status || 500)
   res.json({ error: error.menssage })
 });
 
